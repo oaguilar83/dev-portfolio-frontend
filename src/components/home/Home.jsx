39ae45603bbf5e281cfb3f7977c404d62c8e7cce
oaguilar83 <osc.aguilar83@gmail.com>
2026-01-26 09:09:27 -0800

    add global variables for colors

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,14 +5,14 @@ import styles from './Home.module.css';
 
 function Home() {
   return (
-    <section id="home" className={`${globalStyles.section} ${styles.home_section}`}>
+    <section id="home" className={globalStyles.section}>
       <div className={globalStyles.section_content}>
         <div className={styles.image_text_content}>
           <RoundImage image="src/assets/profile_picture.jpeg" />
           <div className={styles.text}>
             <h1 className={styles.text_title}>Hello, World!<br></br>I'm Oscar Aguilar</h1>
             <p className={styles.text_subtitle}>Software Developer</p>
-            <p className={styles.text_desctiption}>
+            <p className={styles.text_description}>
               Problem-solver who codes. I thrive on dissecting complex challenges, learning whatever it takes to solve them, and emerging with sharper skills than when I started.
             </p>
           </div>
